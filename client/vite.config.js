91cce8d60400c9368de4180a40c7657971b11192
Oussama bouhiri <oussamabouhiri@gmail.com>
2025-05-17 20:22:46 +0100

    Client Update: vite config file resolve object

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      components: path.resolve(__dirname, "./src/components"),
+      pages: path.resolve(__dirname, "./src/pages"),
+      contexts: path.resolve(__dirname, "./src/contexts"),
+      services: path.resolve(__dirname, "./src/services"),
+      utils: path.resolve(__dirname, "./src/utils"),
+    },
   },
   define: {
     // Make process.env available in the client
